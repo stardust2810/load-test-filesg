34bc3cad4b5cdb9a9d120cf63b48b1d9b483415e
Jeffrey Ng <jeffrey_ng@tech.gov.sg>
2022-07-18 22:07:36 +0800

    Initial draft
    Verified working on Mac, removed check for SSL cert

--- locustfile.py
+++ locustfile.py
@@ -1,27 +1,33 @@
 from locust import HttpUser, task, between, TaskSet
 
 class ProcessNumberTask(TaskSet):
+	def on_start(self):
+		self.client.verify = False
+
 	@task(1)
 	def go_ha(self):
-		self.client.get('/health-ambassadors')
+		self.client.get('/health-ambassadors', verify=False)
 
 	@task(2)
 	def go_community(self):
-		self.client.get('/community')
+		self.client.get('/community', verify=False)
 
 	@task(3)
 	def go_career(self):
-		self.client.get('/careers')				
+		self.client.get('/careers', verify=False)				
 
 class Userflow(TaskSet):
+	def on_start(self):
+		self.client.verify = False
+				
 	@task(1)
 	def check_flow(self):
 		#step 1
-		self.client.get('/about/about-us')
+		self.client.get('/about/about-us', verify=False)
 		#step 2
-		self.client.get('/workplace')
+		self.client.get('/workplace', verify=False)
 		#step 3
-		self.client.get('/partners')
+		self.client.get('/partners', verify=False)
 
 class HelloWorldUser(HttpUser):
     wait_time = between(0.5, 2.5)
@@ -29,4 +35,4 @@ class HelloWorldUser(HttpUser):
 
     @task
     def hello_world(self):
-        self.client.get('/')
+        self.client.get('/', verify=False)
