5b953456150160091563a44f951b2af2e63293da
Jeffrey Ng <jeffrey_ng@tech.gov.sg>
2022-07-20 01:57:35 +0800

    Version 1 of load test

--- locustfile.py
+++ locustfile.py
@@ -1,71 +1,132 @@
-from locust import HttpUser, task, between, TaskSet
+from locust import HttpUser, task, between, TaskSet, LoadTestShape
 import json, string, secrets
 
-class ProcessNumberTask(TaskSet):
-	global filesg
+class PortalAccess(TaskSet):
+
 
 	def on_start(self):
-		request_body = {'authCode':'S3002610A','nonce':'mock'}
+		print('PortalAccess|INIT------------------------')
 		self.client.verify = False
-		with self.client.post('/core/auth/mock-login', json=request_body, headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response_token:
-			print(response_token.status_code)
-			print(response_token.cookies)
-			filesg = response_token.cookies.get('filesg-cookie-id')
-			csrf = response_token.cookies.get('filesg-csrf-id')
+
+		#1 go to dev.file.gov.sg
+		#create CSRF token and put into cookie
+		csrf_token = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+		self.client.cookies.set('filesg-csrf-id', csrf_token)
+		print("[1] Go to home page====================")
+		with self.client.post('/', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','cookie':'filesg-csrf-id='+csrf_token}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
 			print("[Dict] +++++++++++++++++++++++++")
 			print(self.client.cookies.get_dict())
-			print(filesg)
-			print(csrf)
-			print(response_token.cookies['filesg-csrf-id'])
-			#self.client.cookies.set('filesg-cookie-id', filesg)
-			
-		print("+++++++++++++++++++++++++")
-		self.client.cookies = response_token.cookies
-		csrf_token = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-		#self.client.cookies['filesg-csrf-id'] = csrf_token
-		self.client.cookies.set('filesg-csrf-id', csrf_token)
-		#self.client.cookies.set_cookie('filesg-csrf-id', csrf_token)
 
-		print(self.client.cookies.get('filesg-cookie-id'))
-		print(self.client.cookies.get('filesg-csrf-id'))
-		
-		cookie='filesg-cookie-id='+self.client.cookies.get('filesg-cookie-id')+'; filesg-csrf-id='+csrf_token
-		self.clients.headers.update({'cookie', cookie})
-		print("+++++++++++++++++++++++++")
+		#2 call get user details to create a session, should return csrf, filesg_cookie
+		print("[2] Get user details===================")
+		with self.client.get('https://www.dev.file.gov.sg/core/auth/user-session-details', headers={'authority':'www.dev.file.gov.sg','accept':'application/json, text/plain, */*','user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','x-csrf-token':csrf_token,'cookie':'filesg-csrf-id='+csrf_token}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())
+			#print(response.text)
 
-	@task(1)
-	def go_ha(self):
-		#self.client.get('/health-ambassadors', verify=False)
-		#self.client.get('/', verify=False)
-		# set cookie for request
-		#self.client.cookies.set('filesg-cookie-id', filesg)
-		cookie='filesg-cookie-id='+self.client.cookies.get('filesg-cookie-id')+'; filesg-csrf-id='+self.client.cookies.get('filesg-csrf-id')
-		print(">>>>>>>>>>>>>>>>>>>>>>>>>")
-		print(self.client.cookies.get('filesg-cookie-id'))
-		print(self.client.cookies.get('filesg-csrf-id'))
-		print(">>>>>>>>>>>>>>>>>>>>>>>>>")
-		#with self.client.post('/auth/user-session-details', cookies=self.client.cookies.get_dict(), headers={'accept':'application/json, text/plain, */*','cookie':cookie, 'content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response_token:
-		with self.client.post('/transaction/activities?sortBy=createdAt&asc=true', cookies={'filesg-cookie-id':self.client.cookies.get('filesg-cookie-id'),'filesg-csrf-id':self.client.cookies.get('filesg-csrf-id')}, headers={'accept':'application/json, text/plain, */*','cookie':cookie, 'content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response_token:
-			print("========================")
-			print(response_token.status_code)
-			print(response_token.text)
-			print(response_token.cookies.get('filesg-cookie-id'))
-			print(response_token.cookies.get('filesg-csrf-id'))
-			print("========================")
 
-			json_var = response_token.json()
-			name=json_var['name']
-			print('*************Name = ' + name)
+		#3 call mock login with payload which returns filesg_cookie
+		print("[3] Call mock login===================")
+		request_body = {'authCode':'S3002610A','nonce':'mock'}
+		with self.client.post('/core/auth/mock-login', json=request_body, headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())
+
+		#4 call get user details (get new session), should return csrf, filesg_cookie
+		print("[4] Get user details again===================")
+
+
+		#5 call activities
+		print("[5] Get activities===================")
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())	
+			#print(response.text)	
+
+		#6 call files
+		print("[6] Get files===================")
+		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())	
+			#print(response.text)		
 
 	@task(2)
-	def go_community(self):
-		#self.client.get('/community', verify=False)
-		self.client.get('/', verify=False)
+	def dashboard(self):
+		print('PortalAccess|dashboard------------------------')
+		print("[1] Go to Dashboard===================")
+		with self.client.get('/home', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='/home') as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())	
+			#print(response.text)
 
-	@task(3)
-	def go_career(self):
+		with self.client.get('/public/__ENV.js', catch_response=True, name='/home') as response:
+			print(response.status_code)
+
+		with self.client.get('/runtime.514b8778286f2bd8.esm.js', catch_response=True, name='/home' ) as response:
+			print(response.status_code)
+
+		with self.client.get('/polyfills.0aab7110e6d8af96.esm.js', catch_response=True, name='/home' ) as response:
+			print(response.status_code)			
+
+		with self.client.get('/main.9810e95c3855dcfe.esm.js', catch_response=True, name='/home' ) as response:
+			print(response.status_code)		
+
+		with self.client.get('/sgds-icons.0dc42d75a6d05f595921.ttf', catch_response=True, name='/home' ) as response:
+			print(response.status_code)				
+
+		with self.client.get('/d36bd175268f5fd4.woff', catch_response=True, name='/home' ) as response:
+			print(response.status_code)		
+
+		with self.client.get('/beta-character.481cd27.svg', catch_response=True, name='/home' ) as response:
+			print(response.status_code)		
+
+		#5 call activities
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='/home' ) as response:
+			print(response.status_code)
+			print(response.text)	
+
+		#6 call files
+		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='/home' ) as response:
+			print(response.status_code)
+			print(response.text)	
+
+		with self.client.get('/ica-logo-only.10b42bf.svg', catch_response=True, name='/home' ) as response:
+			print(response.status_code)	
+
+	@task(1)
+	def all_activities(self):
+		print('PortalAccess|all_activities------------------------')
+		print("[1] Go to ALL Activities===================")
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=10', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True ) as response:
+			print(response.status_code)
+			print(response.cookies)
+			print("[Dict] +++++++++++++++++++++++++")
+			print(self.client.cookies.get_dict())	
+			#print(response.text)
+
+	#@task(2)
+	#def go_community(self):
+	#	print('ProcessNumberTask|go_community------------------------')
+		#self.client.get('/community', verify=False)
+	#	self.client.get('/', verify=False)
+
+	#@task(3)
+	#def go_career(self):
+	#	print('ProcessNumberTask|go_career------------------------')
 		#self.client.get('/careers', verify=False)
-		self.client.get('/', verify=False)				
+	#	self.client.get('/', verify=False)				
 
 class Userflow(TaskSet):
 	def on_start(self):
@@ -84,10 +145,39 @@ class Userflow(TaskSet):
 
 class HelloWorldUser(HttpUser):
     wait_time = between(0.5, 2.5)
-    tasks = [ProcessNumberTask]
+    def on_start(self):
+    	print('One Time Only????????????????????????????????')
 
-    #@task
-    #def hello_world(self):
-    #    self.client.get('/', verify=False)
-    #    print(self.client.cookies.get('filesg-cookie-id'))
-    #    print(self.client.cookies.get('filesg-csrf-id'))
+    tasks = [PortalAccess]
+
+ 
+class StagesShape(LoadTestShape):
+	"""
+	A simply load test shape class that has different user and spawn_rate at
+	different stages.
+	Keyword arguments:
+	stages -- A list of dicts, each representing a stage with the following keys:
+	duration -- When this many seconds pass the test is advanced to the next stage
+	users -- Total user count
+	spawn_rate -- Number of users to start/stop per second
+	stop -- A boolean that can stop that test at a specific stage
+	stop_at_end -- Can be set to stop once all stages have run.
+	The load will start with 0 users and ramp up to 10 users with a spawn rate of 10 until the 5 seconds mark
+	"""
+
+	stages = [
+		{"duration": 5, "users": 10, "spawn_rate": 10},
+		{"duration": 15, "users": 50, "spawn_rate": 10},
+		{"duration": 25, "users": 100, "spawn_rate": 10},
+		{"duration": 60, "users": 1, "spawn_rate": 1}
+	]
+
+	def tick(self):
+		run_time = self.get_run_time()
+
+		for stage in self.stages:
+			if run_time < stage["duration"]:
+				tick_data = (stage["users"], stage["spawn_rate"])
+				return tick_data
+
+		return None
