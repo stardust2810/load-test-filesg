394ad5f8a85b02f48a13a104c28d79cef5bfa0bb
Jeffrey Ng <jeffrey_ng@tech.gov.sg>
2022-07-22 17:20:02 +0800

    Updated to auto read resources instead

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,48 @@
-from locust import HttpUser, task, between, TaskSet, LoadTestShape, events
-import json, string, secrets
+from locust import HttpUser, task, between, TaskSet, LoadTestShape, events, constant
+import json, string, secrets, time
 import logging, sys
 import csv
 import requests
+from lxml import html
+import re
 
 requests.packages.urllib3.disable_warnings() 
 ACCOUNTS = None
+
+#-------------------------------------------
+# For getting embedded resources from a page
+#
+resource_paths = [
+            '//link[@rel="stylesheet"]/@href',
+            '//link[@rel="Stylesheet"]/@href',
+            '//link[@rel="STYLESHEET"]/@href',
+            '//link[@rel="preconnect"]/@href',
+            '//link[@rel="icon"]/@href',
+            '//link[@rel="apple-touch-icon"]/@href',
+            "//script/@src",
+            "//img/@src",
+            "//source/@src",
+            "//embed/@src",
+            "//body/@background",
+            '//input[@type="image"]/@src',
+            '//input[@type="IMAGE"]/@src',
+            '//input[@type="Image"]/@src',
+            "//object/@data",
+            "//frame/@src",
+            "//iframe/@src",
+        ]
+#
+#
+def get_embedded_resources(response_content, filter='.*'):
+    resources = []
+    tree = html.fromstring(response_content)
+    for resource_path in resource_paths:
+        for resource in tree.xpath(resource_path):
+            if re.search(filter, resource): resources.append(resource)
+    return resources
+#
+#-------------------------------------------
+
 
 class PortalAccess(TaskSet):
 	current_user = 'NOT FOUND'
@@ -20,13 +57,12 @@ class PortalAccess(TaskSet):
 			logging.info("No more accounts, reverting to default account S3002610A")
 			self.current_user = 'S3002610A'
 
-
 		#1 go to dev.file.gov.sg
 		#create CSRF token and put into cookie
 		csrf_token = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 		self.client.cookies.set('filesg-csrf-id', csrf_token)
 		logging.info('[%s] Go to home page====================',self.current_user)
-		with self.client.post('/', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','cookie':'filesg-csrf-id='+csrf_token}, catch_response=True, verify=False ) as response:
+		with self.client.post('/', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','cookie':'filesg-csrf-id='+csrf_token}, catch_response=True) as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -34,7 +70,7 @@ class PortalAccess(TaskSet):
 
 		#2 call get user details to create a session, should return csrf, filesg_cookie
 		logging.info('[%s] Get user session====================',self.current_user)
-		with self.client.get('https://www.dev.file.gov.sg/core/auth/user-session-details', headers={'authority':'www.dev.file.gov.sg','accept':'application/json, text/plain, */*','user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','x-csrf-token':csrf_token,'cookie':'filesg-csrf-id='+csrf_token}, catch_response=True, name='get-session', verify=False ) as response:
+		with self.client.get('/core/auth/user-session-details', headers={'authority':'www.dev.file.gov.sg','accept':'application/json, text/plain, */*','user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','x-csrf-token':csrf_token,'cookie':'filesg-csrf-id='+csrf_token}, catch_response=True, name='get-session') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -44,7 +80,7 @@ class PortalAccess(TaskSet):
 		logging.info('[%s] Mock Login====================',self.current_user)
 		#request_body = {'authCode':'S3002610A','nonce':'mock'}
 		request_body = {'authCode':self.current_user,'nonce':'mock'}
-		with self.client.post('/core/auth/mock-login', json=request_body, headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='mock-login', verify=True ) as response:
+		with self.client.post('/core/auth/mock-login', json=request_body, headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='mock-login') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -56,7 +92,7 @@ class PortalAccess(TaskSet):
 
 		#5 call activities
 		logging.info('[%s] Get activities====================',self.current_user)
-		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard/activities', verify=False ) as response:
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard/activities') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -65,7 +101,7 @@ class PortalAccess(TaskSet):
 
 		#6 call files
 		logging.info('[%s] Get files====================',self.current_user)
-		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard/files', verify=False ) as response:
+		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard/files') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -74,43 +110,48 @@ class PortalAccess(TaskSet):
 
 	@task(2)
 	def dashboard(self):
-		self.client.verify = False
 		logging.info('[%s] Return to dashboard====================',self.current_user)
-		with self.client.get('/home', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard', verify=False) as response:
+		with self.client.get('/home', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
 			#logging.debug(self.client.cookies.get_dict())
 			#print(response.text)
+			resources = get_embedded_resources(response.content)
+			for resource in resources:
+				if re.search("^https?://", resource) == None: resource = self.client.base_url + "/" + resource
+				#self.client.get(resource, name="dashboard/resources")
+				self.client.get(resource)
+				
 
-		with self.client.get('/public/__ENV.js', catch_response=True, name='dashboard', verify=False) as response:
+		with self.client.get('/public/__ENV.js', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/runtime.514b8778286f2bd8.esm.js', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/runtime.514b8778286f2bd8.esm.js', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/polyfills.0aab7110e6d8af96.esm.js', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/polyfills.0aab7110e6d8af96.esm.js', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/main.9810e95c3855dcfe.esm.js', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/main.9810e95c3855dcfe.esm.js', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/sgds-icons.0dc42d75a6d05f595921.ttf', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/sgds-icons.0dc42d75a6d05f595921.ttf', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/d36bd175268f5fd4.woff', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/d36bd175268f5fd4.woff', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
-		with self.client.get('/beta-character.481cd27.svg', catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/beta-character.481cd27.svg', catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 
 		#5 call activities
-		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=3', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.text)	
 
 		#6 call files
-		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard', verify=False ) as response:
+		with self.client.get('/core/file/all-files?sortBy=lastViewedAt&asc=false&page=1&limit=5&statuses=active,expired,revoked&ignoreNull=true', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='dashboard') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.text)	
 
@@ -121,7 +162,7 @@ class PortalAccess(TaskSet):
 	def all_activities(self):
 		self.client.verify = False
 		logging.info('[%s] LHS All Activities====================',self.current_user)
-		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=10', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='LHS-menu-all-activities', verify=False ) as response:
+		with self.client.get('/core/transaction/activities?sortBy=createdAt&asc=false&page=1&limit=10', headers={'accept':'application/json, text/plain, */*','content-type':'application/json', 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, catch_response=True, name='LHS-menu-all-activities') as response:
 			logging.debug(response.status_code)
 			logging.debug(response.cookies)
 			#logging.debug("[Dict] +++++++++++++++++++++++++")
@@ -143,11 +184,11 @@ class PortalAccess(TaskSet):
 class Userflow(TaskSet):
 	def on_start(self):
 		#self.client.verify = False
-		self.client.get('/', verify=False)
+		self.client.get('/')
 				
 	@task(1)
 	def check_flow(self):
-		self.client.get('/', verify=False)
+		self.client.get('/')
 		#step 1
 		#self.client.get('/about/about-us', verify=False)
 		#step 2
@@ -186,13 +227,16 @@ class StagesShape(LoadTestShape):
 	"""
 
 	stages = [
-		{"duration": 30, "users": 7, "spawn_rate": 1},
+		{"duration": 160, "users": 2, "spawn_rate": 1},
+		{"duration": 320, "users": 4, "spawn_rate": 1},
+		{"duration": 480, "users": 7, "spawn_rate": 1},
 		#{"duration": 60, "users": 100, "spawn_rate": 10},
 		#{"duration": 120, "users": 200, "spawn_rate": 10},
 		#{"duration": 180, "users": 300, "spawn_rate": 10},
 		#{"duration": 240, "users": 400, "spawn_rate": 10},
-		{"duration": 60, "users": 3, "spawn_rate": 1},
-		{"duration": 90, "users": 1, "spawn_rate": 1}
+		{"duration": 640, "users": 5, "spawn_rate": 1},
+		{"duration": 800, "users": 3, "spawn_rate": 1},
+		{"duration": 960, "users": 1, "spawn_rate": 1}
 	]
 
 	def tick(self):
